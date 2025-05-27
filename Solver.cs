d2aa4138351f3f68bb4c5542b5d9cd09516b8035
ggixiy <anna.543798@gmail.com>
2025-05-27 20:21:53 +0300

    First

--- Solver.cs
+++ Solver.cs
@@ -216,9 +216,11 @@ namespace AlmoustCourseWork
         private static bool IsSafe(int[] state, int row, int col)
         {
             for (int i = 0; i < row; i++)
+            {
                 if (state[i] == -1) continue; //Пропускаємо незаповнені
                 if (state[i] == col || Math.Abs(state[i] - col) == row - i)
-                    return false;
+                return false;
+            }
             return true;
         }
 
